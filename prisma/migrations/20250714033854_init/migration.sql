f5156e78ec77f07d792375fd1e35184b4a115eda
Diego Narrea <diegonrm1999@gmail.com>
2025-07-22 02:43:06 -0500

    removing migrations

--- prisma/migrations/20250714033854_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `percentage` to the `Treatment` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Treatment" ADD COLUMN     "percentage" DOUBLE PRECISION NOT NULL;
--- prisma/migrations/20250720023400_init/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE order_ordernumber_seq;
-ALTER TABLE "Order" ALTER COLUMN "orderNumber" SET DEFAULT nextval('order_ordernumber_seq');
-ALTER SEQUENCE order_ordernumber_seq OWNED BY "Order"."orderNumber";
