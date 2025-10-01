3be8d7abe7242c5059d3cbae3306ab7e4f9c5b4a
Diego Narrea Mori <diego.narrea@ext.blaze.me>
2025-09-30 21:24:04 -0500

    cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
+  async login(
+    @Body() body: { email: string; password: string },
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const user = await this.authService.validateUser(body.email, body.password);
     const loginResponse = await this.authService.login(user);
+    res.cookie('token', loginResponse.token, {
+      httpOnly: true,
+      secure: true, // en prod con https
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 2, 
+    });
+
+    res.cookie('refreshToken', loginResponse.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 30 * 4,
+    });
     return loginResponse;
   }
 
